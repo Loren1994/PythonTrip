919f6e0c063b791096b01f27216d51aa9fad2407
Loren <dayan805@163.com>
2017-08-12 22:31:28 +0800

    add function

--- pers/loren/demo/__init__.py
+++ pers/loren/demo/__init__.py
@@ -1,25 +1,23 @@
 from selenium import webdriver
-import requests
 from bs4 import BeautifulSoup
-import os
-import time
 import pymysql.cursors
+import time
+
+connection = pymysql.connect(host='localhost',
+                             port=3306,
+                             user='root',
+                             password='root',
+                             db='coder_hub',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'}
+web_url = 'http://www.toutiao.com/'
 
 
 class Demo():
-    web_url = 'http://www.toutiao.com/'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'}
-
     @staticmethod
     def db_operate(sql, list):
-        connection = pymysql.connect(host='localhost',
-                                     port=3306,
-                                     user='root',
-                                     password='root',
-                                     db='coder_hub',
-                                     charset='utf8mb4',
-                                     cursorclass=pymysql.cursors.DictCursor)
         try:
             with connection.cursor() as cursor:
                 cursor.executemany(sql, list)
@@ -28,10 +26,10 @@ class Demo():
         finally:
             connection.close()
 
-    def get_pic(self):
+    def get_data(self):
         print("<<<<<<<<getting content>>>>>>>>")
         driver = webdriver.PhantomJS()
-        driver.get(self.web_url)
+        driver.get(web_url)
         tags = BeautifulSoup(driver.page_source, 'lxml').find_all('li', class_='slide-item')
 
         list = []
@@ -39,14 +37,22 @@ class Demo():
             t = tag.a.p.text
             u = tag.a.img['src']
             k = tag.a['href']
+            key = k.split('/')[2]
+            timeStr = time.strftime('%Y-%m-%d', time.localtime(time.time()))
             print(t)
             print(u)
-            print(k)
-            item = (t, u)
+            print(key)
+            print(timeStr)
+            # 0:??? 1:????
+            item = (t, u, key, timeStr,0)
             list.append(item)
-        # sql = "INSERT INTO `tt_home_page` (`content`, `pic_url`) VALUES (%s,%s)"
-        # self.db_operate(sql, list)
+
+        sql = "INSERT INTO `tt_home_page` (`content`, `pic_url`,`click_key`,`create_time`,`type`) VALUES (%s,%s,%s,%s,%s)"
+        self.db_operate(sql, list)
+
+    # ??key????????insert or update
+    # def isExist(list):
 
 
 demo = Demo()
-demo.get_pic()
+demo.get_data()
