27cc9e977c03666b78a980dfe663e7e9524278cb
Loren <dayan805@163.com>
2017-08-16 01:19:48 +0800

    add function

--- pers/loren/demo/__init__.py
+++ pers/loren/demo/__init__.py
@@ -18,6 +18,10 @@ home_insert_sql = "INSERT INTO `tt_home_page` (`content`, `pic_url`,`click_key`,
 home_select_sql = "SELECT `*` FROM `tt_home_page` WHERE `click_key`=%s"
 home_update_sql = "UPDATE tt_home_page SET content = %s, pic_url = %s WHERE click_key = %s"
 
+list_insert_sql = "INSERT INTO `tt_home_list` (`content`, `pic_url`,`click_key`,`create_time`,`type`,`web_time`,`author_name`,`author_head`,`comment_num`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+list_select_sql = "SELECT `*` FROM `tt_home_list` WHERE `click_key`=%s"
+list_update_sql = "UPDATE tt_home_list SET content = %s, pic_url = %s, web_time = %s, comment_num = %s WHERE click_key = %s"
+
 driver = webdriver.PhantomJS()
 
 
@@ -45,13 +49,32 @@ class Demo():
         # 列表数据
         itemList = []
         for itemTag in item_tags:
+            # key
             k = itemTag.find('div', class_='bui-left single-mode-lbox').a['href'].split('/')[2]
+            # 图片
             u = itemTag.find('div', class_='bui-left single-mode-lbox').a.img['src']
-            t = itemTag.find('div', class_='single-mode-rbox').find('span', class_='footer-bar-action')
-            # item = ()
-            # itemList.append(item)
-            print(k + "\n" + u)
-            print(t)
+            # 时间
+            t = itemTag.find('div', class_='single-mode-rbox').find('span', class_='footer-bar-action').text
+            # 标题
+            title = itemTag.find('div', class_='title-box').a.text
+            # 分类
+            type = itemTag.find('div', class_='bui-left footer-bar-left').a.text
+            # 作者头像
+            # head = itemTag.find('div', class_='single-mode-rbox').find('a', class_='footer-bar-action media-avatar')
+            # 作者昵称
+            name = itemTag.find('div', class_='bui-left footer-bar-left').find_all('a',
+                                                                                   class_='footer-bar-action source')
+            na = "" if len(name) == 0 else name[0].text
+            # 评论数
+            num = itemTag.find('div', class_='bui-left footer-bar-left').find_all('a',
+                                                                                  class_='footer-bar-action source')
+            nu = "" if len(num) <= 1 else num[1].text
+            createTime = int(time.time())
+            if '//' in u:
+                item = (title, u.split('//')[1], k, createTime, type, t, na, 'head', nu)
+                itemList.append(item)
+            print(k + "\n" + u + t + "\n" + title + "\n" + type + na + nu)
+        self.isExistList(self, itemList)
         # 轮播图数据
         list = []
         for tag in tags:
@@ -67,7 +90,8 @@ class Demo():
 
     # 所有当天数据
     def getNextPage(self):
-        # 由于图片是懒加载，所以必须滚动一遍到底部才可以拿到图片URL display:none的元素拿到的位置是(0,0)
+        # 由于图片是懒加载，所以必须滚动一遍到底部才可以拿到图片URL 否则是svg+xml的Base64
+        # display:none的元素拿到的位置是(0,0)
         # 滚动到底部三种方式
         # 1、driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         # 2、actions = ActionChains(driver)
@@ -75,12 +99,11 @@ class Demo():
         if self.__isLoop == 0:
             return
         elems = driver.find_elements_by_class_name("lazy-load-img")
+        # elems = driver.find_elements_by_xpath("//img[@lazy='loaded']")
         driver.execute_script("arguments[0].scrollIntoView();", elems[len(elems) - 1])
         time.sleep(3)
         item_tags = BeautifulSoup(driver.page_source, 'lxml').find_all('div', class_='bui-box single-mode')
         for itemTag in item_tags:
-            # k = itemTag.find('div', class_='bui-left single-mode-lbox').a['href'].split('/')[2]
-            # i = itemTag.find('div', class_='bui-left single-mode-lbox').a.img['src']
             u = itemTag.find('div', class_='single-mode-rbox').find('span', class_='footer-bar-action')
             if u.text == ' 1天前':
                 print("超过一天")
@@ -88,12 +111,15 @@ class Demo():
                 print(len(elems))
                 for e in elems:
                     driver.execute_script("arguments[0].scrollIntoView();", e)
-                    time.sleep(0.5)
+                    time.sleep(0.8)
+                # for h in elems_head:
+                #     driver.execute_script("arguments[0].scrollIntoView();", h)
+                #     time.sleep(0.5)
                 print("遍历结束")
                 break
         self.getNextPage()
 
-    # insert or update
+    # home insert or update
     @staticmethod
     def isExist(self, list):
         try:
@@ -102,7 +128,7 @@ class Demo():
                     key = item[2]
                     cursor.execute(home_select_sql, (key))
                     result = cursor.fetchone()
-                    if result == None:
+                    if result is None:
                         self.db_operate(home_insert_sql, item)
                     else:
                         temp = (item[0], item[1], item[2])
@@ -110,6 +136,24 @@ class Demo():
         finally:
             connection.close()
             print('isExist finally')
+
+    # list insert or update
+    @staticmethod
+    def isExistList(self, list):
+        try:
+            with connection.cursor() as cursor:
+                for item in list:
+                    key = item[2]
+                    cursor.execute(list_select_sql, (key))
+                    result = cursor.fetchone()
+                    if result is None:
+                        self.db_operate(list_insert_sql, item)
+                    else:
+                        temp = (item[0], item[1], item[5], item[8], item[2])
+                        self.db_operate(list_update_sql, temp)
+        finally:
+            # connection.close()
+            print('isExistList finally')
 
 
 demo = Demo()
